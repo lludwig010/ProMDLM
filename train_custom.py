606199eaef7b9f1f42db058d6151cdec2d4e0c9a
ghassanabboud <ghassan.abboud@epfl.ch>
2025-04-18 00:54:13 -0400

    logging everywhere, add config files, ready for first train

--- train_custom.py
+++ train_custom.py
@@ -14,6 +14,7 @@ import logging
 import torch
 import os
 import time
+import shutil
 
 class Trainer:
     def __init__(self, model, optimizer, loss_function, epochs, train_loader, val_loader, max_timesteps, batch_size, seq_len, vocab_size, device, output_dir, job_name="TEST", validation = False):
@@ -32,6 +33,7 @@ class Trainer:
         self.output_dir = output_dir
         self.job_name = job_name
         self.validation = validation
+        self.logger = logging.getLogger()
 
 
     def train_loop_fullDiff(self):
@@ -44,7 +46,7 @@ class Trainer:
 
         for self.epoch in range(self.epochs):
 
-            print(f"doing epoch: {self.epoch}")
+            self.logger.info(f"doing epoch: {self.epoch} out of {self.epochs}")
 
             self.model.train()
             train_total_loss = 0.0
@@ -55,7 +57,7 @@ class Trainer:
             self.model.train()
             
             for i, batched_sequences_tokenized in enumerate(self.train_loader):
-                print(f"Training batch: {i}/{nb_batches_train}")
+                self.logger.info(f"Training batch: {i}/{nb_batches_train}")
 
                 batch_size = batched_sequences_tokenized.shape
                 size_to_mask = batch_size[0]
@@ -67,7 +69,7 @@ class Trainer:
                 # sample time step
                 t = torch.randint(0, self.max_timesteps, (1,)).item()
 
-                #print(f"sampled timestep {t}")
+                self.logger.info(f"sampled timestep {t}")
 
                 batch_masks = noise_schedule(self.max_timesteps, t, size_to_mask, self.seq_len)
                 masked_batch_seq, batch_masks = apply_noise(batch_masks, batched_sequences_tokenized, t)
@@ -75,7 +77,7 @@ class Trainer:
                 batch_loss = scheduler_loss_fn(batch_pred_tokens, batched_sequences_tokenized, masked_batch_seq, self.vocab_size)
 
                 batch_loss.backward()
-                #print(f"batch loss: {batch_loss}")
+                #self.logger.info(f"batch loss: {batch_loss}")
                 self.optimizer.step()
 
                 train_losses_batch.append(batch_loss.item())
@@ -83,17 +85,17 @@ class Trainer:
                 train_total_loss += batch_loss.item()
             
             avg_train_loss = train_total_loss / len(self.train_loader)
-            print(f"epoch loss train: {avg_train_loss}")
+            self.logger.info(f"epoch loss train: {avg_train_loss}")
             train_losses.append(avg_train_loss)
 
             if self.validation:
                 self.model.eval()
-                print("Validation")
+                self.logger.info("Validation")
                 torch.cuda.empty_cache()
                 nb_batches_val = len(self.val_loader)
                 
                 for i, batched_sequences_tokenized in enumerate(self.val_loader):
-                    print(f"validation batch: {i}/{nb_batches_val}")
+                    self.logger.info(f"validation batch: {i}/{nb_batches_val}")
 
                     batch_size = batched_sequences_tokenized.shape
                     size_to_mask = batch_size[0]
@@ -114,7 +116,7 @@ class Trainer:
                     val_losses_batch.append(val_batch_loss.item()) 
 
                 avg_val_loss = val_total_loss/len(self.val_loader) 
-                print(f"epoch loss val: {avg_train_loss}")
+                self.logger.info(f"epoch loss val: {avg_train_loss}")
                 val_losses.append(avg_val_loss)
 
         plot(self.job_name, train_losses_batch, val_losses_batch, train_losses, val_losses, self.output_dir)
@@ -134,7 +136,7 @@ class Trainer:
 
             for self.epoch in range(self.epochs):
 
-                print(f"doing epoch: {self.epoch}")
+                self.logger.info(f"doing epoch: {self.epoch} out of {self.epochs}")
 
                 self.model.train()
                 train_total_loss = 0.0
@@ -145,7 +147,7 @@ class Trainer:
                 self.model.train()
                 
                 for i, batched_sequences_tokenized in enumerate(self.train_loader):
-                    print(f"Training batch: {i}/{nb_batches_train}")
+                    self.logger.info(f"Training batch: {i}/{nb_batches_train}")
 
                     batch_size = batched_sequences_tokenized.shape
                     size_to_mask = batch_size[0]
@@ -162,7 +164,7 @@ class Trainer:
                         # second half of training, random noise sampling
                         t = torch.randint(0, self.max_timesteps, (1,)).item()
 
-                    print(f"sampled timestep {t}")
+                    self.logger.info(f"sampled timestep {t}")
 
                     batch_masks = noise_schedule(self.max_timesteps, t, size_to_mask, self.seq_len)
                     masked_batch_seq, batch_masks = apply_noise(batch_masks, batched_sequences_tokenized, t)
@@ -170,7 +172,7 @@ class Trainer:
                     batch_loss = scheduler_loss_fn(batch_pred_tokens, batched_sequences_tokenized, masked_batch_seq, self.vocab_size)
 
                     batch_loss.backward()
-                    #print(f"batch loss: {batch_loss}")
+                    #self.logger.info(f"batch loss: {batch_loss}")
                     self.optimizer.step()
 
                     train_losses_batch.append(batch_loss.item())
@@ -178,17 +180,17 @@ class Trainer:
                     train_total_loss += batch_loss.item()
                 
                 avg_train_loss = train_total_loss / len(self.train_loader)
-                print(f"epoch loss train: {avg_train_loss}")
+                self.logger.info(f"epoch loss train: {avg_train_loss}")
                 train_losses.append(avg_train_loss)
 
                 if self.validation:
                     self.model.eval()
-                    print("Validation")
+                    self.logger.info("Validation")
                     torch.cuda.empty_cache()
                     nb_batches_val = len(self.val_loader)
                 
                     for i, batched_sequences_tokenized in enumerate(self.val_loader):
-                        print(f"validation batch: {i}/{nb_batches_val}")
+                        self.logger.info(f"validation batch: {i}/{nb_batches_val}")
 
                         batch_size = batched_sequences_tokenized.shape
                         size_to_mask = batch_size[0]
@@ -213,7 +215,7 @@ class Trainer:
                         val_losses_batch.append(val_batch_loss.item()) 
 
                     avg_val_loss = val_total_loss/len(self.val_loader) 
-                    print(f"epoch loss val: {avg_train_loss}")
+                    self.logger.info(f"epoch loss val: {avg_train_loss}")
                     val_losses.append(avg_val_loss)
 
             plot(self.job_name, train_losses_batch, val_losses_batch, train_losses, val_losses, self.output_dir)
@@ -231,23 +233,13 @@ class Trainer:
         train_losses_batch = []
         val_losses_batch=[]
 
-        
 
-        logging.basicConfig(
-            filename='training_increment_ghassan.log',
-            filemode='w',  # 'a' for append, 'w' to overwrite each time
-            format='%(asctime)s - %(levelname)s - %(message)s',
-            level=logging.INFO
-        )
-        logger = logging.getLogger()
-
-        logger.info("num train data points: %d", len(self.train_loader))
+        self.logger.info("num train data points: %d", len(self.train_loader))
 
         
         for epoch in range(self.epochs):
 
-            print(f"doing epoch: {epoch}")
-            logger.info(f"Starting epoch: {epoch}")
+            self.logger.info(f"doing epoch: {epoch} out of {self.epochs}")
 
             self.model.train()
             train_total_loss = 0.0
@@ -258,8 +250,7 @@ class Trainer:
             
             for i, batched_sequences_tokenized in enumerate(self.train_loader):
                 
-                print(f"Training batch {i}/{nb_batch_train}")
-                logger.info(f"Training batch {i}/{nb_batch_train}")
+                self.logger.info(f"Training batch {i}/{nb_batch_train}")
 
                 batch_size = batched_sequences_tokenized.shape
 
@@ -271,15 +262,16 @@ class Trainer:
 
                 # sample time step based off of the epoch training is on 
                 # timestep is proportional to current epoch on
-                timestep = (epoch/self.epochs) * self.max_timesteps
+                timestep = ((epoch/self.epochs) + (i/nb_batch_train)*(1/self.epochs))* self.max_timesteps
 
                 # define range for possible timesteps to sample between. upper and lower bound are 10% away from current timestep
                 low_timestep = max(0, timestep - 0.1 * self.max_timesteps)
                 max_timestep = min(self.max_timesteps, timestep + 0.1 * self.max_timesteps)
 
                 t = torch.randint(int(low_timestep), int(max_timestep), (1,)).item()
+                self.logger.info(f"sampled timestep {t}")
 
-                #print(f"sampled timestep {t}")
+                #self.logger.info(f"sampled timestep {t}")
 
                 batch_masks = noise_schedule(self.max_timesteps, t, size_to_mask, self.seq_len)
                 masked_batch_seq, batch_masks = apply_noise(batch_masks, batched_sequences_tokenized, t)
@@ -287,7 +279,7 @@ class Trainer:
                 batch_loss = scheduler_loss_fn(batch_pred_tokens, batched_sequences_tokenized, masked_batch_seq, self.vocab_size)
 
                 batch_loss.backward()
-                #print(f"batch loss: {batch_loss}")
+                #self.logger.info(f"batch loss: {batch_loss}")
                 self.optimizer.step()
 
                 train_losses_batch.append(batch_loss.item())
@@ -295,21 +287,19 @@ class Trainer:
                 train_total_loss += batch_loss.item()
             
             avg_train_loss = train_total_loss / len(self.train_loader)
-            print(f"epoch loss train: {avg_train_loss}")
-            logger.info(f"Epoch {epoch} average training loss: {avg_train_loss:.4f}")
+            self.logger.info(f"epoch loss train: {avg_train_loss}")
             train_losses.append(avg_train_loss)
 
             if self.validation:
 
                 self.model.eval()
-                print("Validation")
+                self.logger.info("Validation")
                 torch.cuda.empty_cache()
                 nb_batches_val = len(self.val_loader)
 
                 torch.cuda.empty_cache()
                 for i, batched_sequences_tokenized in enumerate(self.val_loader):
-                    print(f"validation batch: {i}/{nb_batches_val}")
-                    logger.info(f"Validation batch: {i}/{nb_batches_val}")
+                    self.logger.info(f"validation batch: {i}/{nb_batches_val}")
 
                     batch_size = batched_sequences_tokenized.shape
                     size_to_mask = batch_size[0]
@@ -326,6 +316,7 @@ class Trainer:
                     max_timestep = min(self.max_timesteps, timestep + 0.1 * self.max_timesteps)
 
                     t = torch.randint(int(low_timestep), int(max_timestep), (1,)).item()
+                    
 
                     batch_masks = noise_schedule(self.max_timesteps, t, size_to_mask, self.seq_len)
                     masked_batch_seq, batch_masks = apply_noise(batch_masks, batched_sequences_tokenized, t)
@@ -336,14 +327,14 @@ class Trainer:
                     val_losses_batch.append(val_batch_loss.item()) 
             
                 avg_val_loss = val_total_loss/len(self.val_loader) 
-                print(f"epoch loss val: {avg_train_loss}")
+                self.logger.info(f"epoch loss val: {avg_train_loss}")
                 val_losses.append(avg_val_loss)
     
 
         plot(self.job_name, train_losses_batch, val_losses_batch, train_losses, val_losses, self.output_dir)
 
         torch.save(self.model, self.output_dir + f'/{self.job_name}_weights.pth')
-        logger.info("Training completed and model saved.")
+        self.logger.info("Training completed and model saved.")
         return train_losses, self.model
     
 
@@ -387,8 +378,16 @@ def plot(job_name, batch_train_losses, batch_val_losses, epoch_train_loses, epoc
 
 
 def train_main():
-    config = OmegaConf.load("configs/g_config.yaml")
+
+    start = time.perf_counter()
+
+
+    config_path = "configs/training_two_stage.yaml"
+    config = OmegaConf.load(config_path)
     os.makedirs(config.paths.output_dir, exist_ok=True)
+    shutil.copy(config_path, os.path.join(config.paths.output_dir, "config_used.yaml")) #copy config file to output dir
+
+
 
     train_file_pkl = config.paths.train_file
     val_file_pkl = config.paths.val_file
@@ -403,10 +402,19 @@ def train_main():
     weight_decay = config.training.weight_decay
     # added scheme to choose what kind of training
     training_scheme = config.training.scheme
-    # added job name for naming runs
-    job_name = config.paths.job_name
     output_dir = config.paths.output_dir
     validation = config.training.validation
+    # no more need to specify both job name and output dir
+    job_name = os.path.basename(output_dir.rstrip('/'))
+
+    # Set up logging
+    logging.basicConfig(
+        filename=os.path.join(output_dir,f'{job_name}.log'),
+        filemode='w',  # 'a' for append, 'w' to overwrite each time
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    logger = logging.getLogger()
 
 
 
@@ -424,8 +432,8 @@ def train_main():
     val_dataset = CustomDataset(val_file_pkl, max_datapoints= 100)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
     
-    print(f"Number of training samples: {len(train_dataset)}")
-    print(f"Number of validation samples: {len(val_dataset)}")
+    logger.info(f"Number of training samples: {len(train_dataset)}")
+    logger.info(f"Number of validation samples: {len(val_dataset)}")
 
     trainer = Trainer(
         model, optimizer, loss, num_epochs, train_loader, val_loader,
@@ -435,23 +443,23 @@ def train_main():
 
     # default training scheme
     if training_scheme == 1:
-        print("Training scheme 1: full diffusion")
+        logger.info("Training scheme 1: full diffusion")
         train_losses, model = trainer.train_loop_fullDiff()
     # incremental diffusion
     elif training_scheme == 2:
-        print("Training scheme 2: incremental diffusion")
+        logger.info("Training scheme 2: incremental diffusion")
         train_losses, model = trainer.train_increment_diffusion()
     elif training_scheme == 3:
-        print("Training scheme 3: two stage training")
+        logger.info("Training scheme 3: two stage training")
         train_losses, model = trainer.train_loop_two_stage()
 
-    print(f"Training complete. Saving model under {output_dir}")
+    logger.info(f"Training complete. Saving model under {output_dir}")
+    logger.info(f"Training took {time.perf_counter() - start:.2f} seconds")
     return train_losses, model
 
 
 
 if __name__ == "__main__":
-    start = time.perf_counter()
+
     torch.cuda.empty_cache()
     train_main()
-    print(f"Training took {time.perf_counter() - start} seconds")
