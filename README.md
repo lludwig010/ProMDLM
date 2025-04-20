a447e21ed96c91e46f10589dd69a51e38d18f4ad
ghassanabboud <ghassan.abboud@epfl.ch>
2025-04-20 18:36:17 -0400

    include blast for evaluation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 first run prepare.py to generate the pkl array of sequences ready to go.
 then train using train_custom.py
+
+### Sequence Diversity Evaluation using Blastp
+
+Blastp is a tool for aligning and comparing protein sequences. We use Blastp to evaluate the diversity of generated sequences by aligning them to the training set. We here provide a brief tutorial to reproduce that analysis. 
---
+++ utils.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+
+def csv_to_fasta(csv_file, fasta_file, sequence_col, id_col):
+    """
+    Transforms a CSV file with sequences into a FASTA file.
+
+    Args:
+        csv_file (str): Path to the input CSV file.
+        fasta_file (str): Path to the output FASTA file.
+        sequence_col (str): Column name containing sequences.
+        id_col (str): Column name containing unique identifiers.
+    """
+    # Read the CSV file
+    df = pd.read_csv(csv_file)
+
+    # Open the FASTA file for writing
+    with open(fasta_file, 'w') as fasta:
+        for _, row in df.iterrows():
+            # Write the FASTA format
+            fasta.write(f">{row[id_col]}\n{row[sequence_col]}\n")
