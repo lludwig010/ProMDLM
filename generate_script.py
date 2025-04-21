9f31068dc9f64aec8902f2db16aad986daf81f78
ghassanabboud <ghassan.abboud@epfl.ch>
2025-04-21 00:39:45 -0400

    final generation

--- generate_script.py
+++ generate_script.py
@@ -7,15 +7,19 @@ import os
 
 
 model_paths = ["training_results/second_attempt_big_increment_training/second_attempt_big_increment_training_weights.pth",
-              "training_results/second_attempt_big_two_stage_training/second_attempt_big_two_stage_training_weights.pth"]
-save_names = ["increment", "two_stage"]
-output_dir = "generated_sequences"
+              "training_results/second_attempt_big_two_stage_training/second_attempt_big_two_stage_training_weights.pth",
+              "/home/jtso3/ghassan/ProMDLM/training_results/fulldiff_weights.pth"]
+save_names = ["increment", "two_stage", "fulldiff"]
+output_dir = "generated_sequences_215aa_long"
 os.makedirs(output_dir, exist_ok=True)
 
 device = torch.device("cuda")
-max_iter=500
-generation_length = 150
-nb_generated_sequences = 100
+
+#PARAMETERS
+max_iter= 500
+generation_length = 180
+nb_generated_sequences = 10
+resample_ratio = 0.2
 
 for model_path, save_name in zip(model_paths, save_names):
 
@@ -23,7 +27,6 @@ for model_path, save_name in zip(model_paths, save_names):
     model = torch.load(model_path, weights_only=False)
     model = model.to(device)
     tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t30_150M_UR50D")
-
 
     for temperature in [0.5,1,1.5]:
         print(f"Generating sequences with temperature {temperature} for model {save_name}")
@@ -34,7 +37,7 @@ for model_path, save_name in zip(model_paths, save_names):
         batch = {
             "input_ids": input_ids,
         }
-        output = model.generate(batch, max_iter=500, temperature=0.5, resample_ratio=0.2, sampling_strategy="vanilla")
+        output = model.generate(batch, max_iter=max_iter, temperature=temperature, resample_ratio=resample_ratio, sampling_strategy="vanilla")
 
         for i in range(nb_generated_sequences):
             decode = tokenizer.decode(output[0][i], skip_special_tokens=True)
